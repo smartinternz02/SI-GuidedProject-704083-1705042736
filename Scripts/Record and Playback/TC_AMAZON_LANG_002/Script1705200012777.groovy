62c23a09b5ae62c8c3b3722dd014fe6351db2a62
Chetan <chetankumarpulipati4@gmail.com>
2024-01-15 09:38:07 +0530

    TC_AMAZON_LANG_002

--- Scripts/Record and Playback/TC_AMAZON_LANG_002/Script1705200012777.groovy
+++ Scripts/Record and Playback/TC_AMAZON_LANG_002/Script1705200012777.groovy
@@ -17,3 +17,43 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.amazon.com/')
+
+WebUI.click(findTestObject('Object Repository/TC_AMAZON_LANG_002/Page_Amazon.com. Spend less. Smile more/a_EN'))
+
+WebUI.click(findTestObject('Object Repository/TC_AMAZON_LANG_002/Page_Change Language  Currency Settings/label_espaol -        ES                   _44edb8'))
+
+WebUI.click(findTestObject('Object Repository/TC_AMAZON_LANG_002/Page_Change Language  Currency Settings/input_Cancelar_a-button-input'))
+
+WebUI.click(findTestObject('Object Repository/TC_AMAZON_LANG_002/Page_Amazon.com. Gasta menos. Sonre ms/a_ES'))
+
+WebUI.click(findTestObject('Object Repository/TC_AMAZON_LANG_002/Page_Cambiar configuracin de idioma y moneda/label_portugus -        PT                 _69c65f'))
+
+WebUI.click(findTestObject('Object Repository/TC_AMAZON_LANG_002/Page_Cambiar configuracin de idioma y moneda/input_Cancelar_a-button-input'))
+
+WebUI.click(findTestObject('Object Repository/TC_AMAZON_LANG_002/Page_Amazon.com. Spend less. Smile more/span_English - EN'))
+
+WebUI.click(findTestObject('Object Repository/TC_AMAZON_LANG_002/Page_Amazon.com. Spend less. Smile more/span_Hello, sign in'))
+
+WebUI.setText(findTestObject('Object Repository/TC_AMAZON_LANG_002/Page_Amazon Sign-In/input_Email or mobile phone number_email'), 
+    'chetankumarpulipati4@gmail.com')
+
+WebUI.click(findTestObject('Object Repository/TC_AMAZON_LANG_002/Page_Amazon Sign-In/input_Enter your password_signInSubmit'))
+
+WebUI.click(findTestObject('Object Repository/TC_AMAZON_LANG_002/Page_Amazon.com. Spend less. Smile more/span_Sign Out'))
+
+WebUI.click(findTestObject('Object Repository/TC_AMAZON_LANG_002/Page_Amazon.com. Spend less. Smile more/span_EN'))
+
+WebUI.click(findTestObject('Object Repository/TC_AMAZON_LANG_002/Page_Change Language  Currency Settings/label_Deutsch -        DE                  _ff571f'))
+
+WebUI.click(findTestObject('Object Repository/TC_AMAZON_LANG_002/Page_Change Language  Currency Settings/input_Cancelar_a-button-input'))
+
+WebUI.setText(findTestObject('Object Repository/TC_AMAZON_LANG_002/Page_Amazon.com. Spend less. Smile more/input_Suche Amazon_field-keywords'), 
+    'kar')
+
+WebUI.click(findTestObject('Object Repository/TC_AMAZON_LANG_002/Page_Amazon.com. Spend less. Smile more/input_Suche Amazon_nav-search-submit-button'))
+
+WebUI.closeBrowser()
+
